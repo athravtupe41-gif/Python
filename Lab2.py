e5482496a3337ce1aefd14417d0e28c0a90d43de
athravtupe41-gif <athravtupe41@gmail.com>
2026-09-25 10:37:46 +0530

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,48 @@
+train_name = "Rajdhani Express"
+train_number = 12951
+source_station = "Mumbai"
+destination_station = "Delhi"
+ticket_price = 2450.75
+
+print("Welcome to Indian Railway Reservation System")
+print("Train Name :", train_name)
+print("Train Number :", train_number)
+print("Source Station :", source_station)
+print("Destination Station :", destination_station)
+
+print(type(train_name))
+print(type(train_number))
+print(type(ticket_price))
+
+passenger_name = "Rahul Sharma"
+seat_number = "B2-45"
+is_confirmed = True
+
+print(type(ticket_price))
+ticket_price_int = int(ticket_price)
+print(type(ticket_price_int))
+
+print(ticket_price_int)
+
+print(ticket_price)
+
+train_number_str = str(train_number)
+print(type(train_number_str))
+
+#Railway Reservation Output system
+
+#Passenger Information 
+passenger_name = "Rahul Sharma"
+
+#Ticket Confirmation Status
+confirmation_status = True
+
+passenger_name = "Rahul Sharma"
+seat_number = "B2-45"
+confirmation_status = "confirmed"
+
+print("-------------Railway Ticket-------------")
+print("Passenger Name :", passenger_name)
+print("Seat number :", seat_number)
+print("Ticket Status :", confirmation_status)
+print("-------------------------")
